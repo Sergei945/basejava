f6707775e95f198bca673344503ebd4753af6a98
Avel945 <fedor945@yandex.ru>
2023-08-19 23:35:50 +0300

    My realise methods (clear, save, get,delete, getAll, size) for homeWork lesson1 baseJava and use OOP technology in this project

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,52 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int countResume = 0;
 
-    void clear() {
+    public void clear() {
+        Arrays.fill(storage, 0, countResume, null);
+        countResume = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        storage[countResume] = r;
+        countResume++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                System.out.println("\nРезюме " + storage[i] + " удалено");
+                System.arraycopy(storage, i + 1, storage, i, countResume - 1 - i);
+                countResume--;
+                storage[countResume] = null;
+                return;
+            }
+            System.out.println("Нет такого резюме");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, countResume);
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return countResume;
     }
 }
